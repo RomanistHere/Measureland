c183278e4be3f9be1c83820e1651f01297695368
RomanistHere <RomanistHere@gmail.com>
2021-09-07 20:29:39 +0300

    validate input

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -4,24 +4,36 @@
     export let id = 'current-email';
 
     let shouldShowPassword = false;
+    let isInputValid = true;
+    let isInputActive = false;
+    let hasTypingStarted = false;
 
     const validateEmail = email =>
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 
     const validatePass = pass =>
-        pass.length > 6 && pass.length < 255
+        pass.length > 6 && pass.length < 255;
 
     const changeInputType = () =>
         shouldShowPassword = !shouldShowPassword;
 
     const onBlur = e => {
+        isInputActive = false;
         const inputContent = e.currentTarget.value;
-        console.log(inputContent)
+
+        if (type === 'email') {
+            const email = inputContent.toLowerCase();
+            isInputValid = validateEmail(email);
+        } else if (type === 'password') {
+            isInputValid = validatePass(inputContent);
+        }
     }
 
     const onInput = e => {
+        isInputValid = false;
+        hasTypingStarted = true;
+        isInputActive = true;
         const inputContent = e.currentTarget.value;
-        console.log(inputContent)
     }
 </script>
 
@@ -32,11 +44,26 @@
             <a href={"#"} class="form__label_help" on:click|preventDefault={changeInputType}>Show/hide</a>
         {/if}
     </label>
-    <input class="form__input" type="{shouldShowPassword ? 'text' : type}" id="{id}" autocomplete='{id}' on:blur={onBlur} on:input={onInput}>
+    <input
+        class="form__input {!isInputValid && !isInputActive ? 'form__input-error' : ''} {isInputValid && hasTypingStarted && !isInputActive ? 'form__input-success' : ''}"
+        type="{shouldShowPassword ? 'text' : type}"
+        id="{id}"
+        autocomplete='{id}'
+        on:blur={onBlur}
+        on:input={onInput}
+    >
 
-    {#if type === 'email'}
-        <span class="form__error">Wrong email format! Example: yournick@mail.com</span>
-    {:else if type === 'password'}
-        <span class="form__error">Password should be longer than 6 characters!</span>
+    {#if !isInputValid && !isInputActive}
+        {#if type === 'email'}
+            <span class="form__error">Wrong email format! Example: yournick@mail.com</span>
+        {:else if type === 'password'}
+            <span class="form__error">Password should be longer than 6 characters!</span>
+        {/if}
     {/if}
 </div>
+
+<style>
+    .form__error {
+        opacity: 1;
+    }
+</style>
