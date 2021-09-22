c34d9bbec68087ed72ffec42d11b12ca2d6c5271
RomanistHere <RomanistHere@gmail.com>
2021-09-22 16:47:56 +0300

    main page

--- src/lib/components/Overlay/components/StartScreen/Slide.svelte
+++ src/lib/components/Overlay/components/StartScreen/Slide.svelte
@@ -1,11 +1,22 @@
 <script>
+    import MainButton from '../MainButton.svelte';
+
     export let title = '';
     export let text = '';
+    export let text2 = '';
+    export let btnText = '';
+    export let btnAction = () => {};
 </script>
 
 <section class="flex items-center justify-center w-full h-full">
-    <div class="flex items-center justify-center flex-wrap">
-        <h1 class="text-5xl px-20 w-full text-center mb-10">{title}</h1>
-        <p class="text-2xl px-20 max-w-xl">{text}</p>
+    <div class="max-w-xl">
+        <h1 class="text-5xl mb-10">{title}</h1>
+        <p class="text-2xl">{text}</p>
+        {#if text2}
+            <p class="text-2xl mt-5">{text2}</p>
+        {/if}
+        {#if btnText}
+            <MainButton text={btnText} action={btnAction} className='inline-block mt-5 px-10' />
+        {/if}
     </div>
 </section>
--- src/lib/components/Overlay/components/StartScreen/StartScreen.svelte
+++ src/lib/components/Overlay/components/StartScreen/StartScreen.svelte
@@ -6,23 +6,46 @@
     import 'swiper/css/pagination';
 
     import Slide from './Slide.svelte';
+    import MainButton from '../MainButton.svelte';
+    import SecondaryButton from '../SecondaryButton.svelte';
 
     SwiperCore.use([Mousewheel, Pagination]);
 
     let shouldShowScrollCaption = true;
 
-    const slides = [{
-        title: 'We are useful',
-        text: 'We help people by saving time, energy and reducing the stress while looking for a place to stay.'
+    const contentSlides = [{
+        title: `We are simple and useful`,
+        btnText: `Check good water around`,
+        btnAction: () => {
+            // open filters
+            // remove layer
+        },
+        text: `We help people by saving time, energy and reducing the stress 🤯 while looking for a place to stay.`,
+        text2: `Check the districts with good or bad water quality 💧 in the town (by townsman's opionion) in literally three clicks from now.`
     },{
-        title: 'We are community driven',
-        text: 'There are no thing like promoted rating or paid review. Content created for people by people.'
+        title:`We are community driven`,
+        btnText: `Check our community guides`,
+        btnAction: () => {
+            // link to github
+        },
+        text: `There are no thing like promoted rating or paid review here. Content created for people by people.`,
+        text2: `Become the creator. We will help you 👋`
     },{
-        title: 'We are non-commercial',
-        text: 'We would love your attention, but making the money is not the goal of this service.'
+        title: `We are non-commercial`,
+        btnText: `Support us`,
+        btnAction: () => {
+            // link to support
+        },
+        text: `We would love your support, but making the money is not the goal of this service.`,
+        text2: `There are more important things in life 🌼`
     },{
-        title: 'We are privacy oriented',
-        text: 'Do not trust our words. Trust our code. It is open source. We do not share personal data.'
+        title: `We are privacy oriented`,
+        btnText: `Check our code`,
+        btnAction: () => {
+            // link to github
+        },
+        text: `Do not trust our words 🙈 Trust our code 🙉 It is open source.`,
+        text2: `We can't sell or lose your personal data because we don't collect it 🙊`
     }];
 
     const onSlideChange = () => {
@@ -41,9 +64,29 @@
         on:slideChange={onSlideChange}
         on:swiper={(e) => console.log(e.detail[0])}
     >
-        {#each slides as { title, text }}
+        <SwiperSlide>
+            <section class="flex items-center justify-center w-full h-full">
+                <div class="max-w-xl">
+                    <h1 class="text-6xl mb-5">
+                        Welcome to
+                        <span class="text-7xl font-bold">
+                            Measureland
+                        </span>
+                    </h1>
+                    <p class="text-2xl mb-5">
+                        Free service we built to help you share your residential experiences and find the best place 🏡 to rent or buy.
+                    </p>
+                    <div class="flex items-center justify-left">
+                        <SecondaryButton text="Continue without signing in" className="mr-5" />
+                        <MainButton text="Create account" className='block' />
+                    </div>
+                </div>
+            </section>
+        </SwiperSlide>
+
+        {#each contentSlides as slideData}
             <SwiperSlide>
-                <Slide { title } { text } />
+                <Slide { ...slideData } />
             </SwiperSlide>
         {/each}
     </Swiper>
@@ -68,6 +111,10 @@
 
     :global(.swiper) {
         height: 100%;
+    }
+
+    :global(.swiper-pagination-bullet) {
+        background: var(--text-color);
     }
 
     :global(.swiper-pagination-bullet-active) {
