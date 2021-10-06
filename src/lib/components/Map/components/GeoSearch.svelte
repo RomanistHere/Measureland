22ea44e2de8f4dafbd2b5326fa0aeb8ea93bf067
RomanistHere <RomanistHere@gmail.com>
2021-10-06 13:19:42 +0300

    translate geosearch

--- src/lib/components/Map/components/GeoSearch.svelte
+++ src/lib/components/Map/components/GeoSearch.svelte
@@ -2,9 +2,11 @@
     import L from 'leaflet';
     import * as ELG from 'esri-leaflet-geocoder';
     import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
+    import { _ } from 'svelte-i18n';
 
-    import { mapReference, geocodeServiceReference } from "../../../../stores/references.js";
+    import { mapReference, geocodeServiceReference } from '../../../../stores/references.js';
 
+    // TODO: probably move to back-end
     const esriApiKey = 'AAPKdec033141fc049a1936e3862bd2fec4ce1WeDmCkYfNW9w7DMLrt7bfPVl8vWPRistJ8w-fEzIg0u4I6uVRL1tIxuqajfw7Q';
     const geocodeService = new ELG.geocodeService({ apikey: esriApiKey });
     const map = $mapReference;
@@ -17,13 +19,11 @@
                 apikey: esriApiKey
             })
         ],
-        // TODO: language
-        placeholder: 'en' === 'en' ? 'Search for places or addresses' : 'Поиск по местам и адресам',
+        placeholder: $_('_.searchPlaceholder'),
         useMapBounds: false,
-    }).addTo(map)
+    }).addTo(map);
 
-    searchControl.on("results", function (data) {
-        console.log(data)
-        map.setView(data.results[0].latlng, 17)
-    })
+    searchControl.on('results', data => {
+        map.setView(data.results[0].latlng, 17);
+    });
 </script>
