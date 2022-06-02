b5b151ebe7993e6d91ce46d16f034d1e3428a931
RomanistHere <RomanistHere@gmail.com>
2022-06-02 17:53:44 +0300

    fix: styles

--- src/lib/components/SEO/SEO.svelte
+++ src/lib/components/SEO/SEO.svelte
@@ -39,7 +39,7 @@
 
     <meta http-equiv="Content-Security-Policy" content="
         base-uri 'self';
-        connect-src 'self' {API_DOMAIN} plausible.io api.mapbox.com *.openstreetmap.org geocode-api.arcgis.com api.waqi.info en.wikipedia.org;
+        connect-src 'self' {API_DOMAIN} plausible.io api.mapbox.com *.openstreetmap.org api.waqi.info en.wikipedia.org;
         default-src 'self';
         font-src 'self' data:;
         img-src 'self' api.mapbox.com *.openstreetmap.org data:;
